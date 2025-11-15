5602e4862c48befce7ae6a17d7c5fece5b2b2c6f
MstJonas <109620968+MstJonas@users.noreply.github.com>
2025-11-14 22:34:30 -0300

    Add files via upload
    Um simples e básico programa em 'for'

---
+++ Número é ou não Primo.1!.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/maiko/OneDrive/Documentos/Meus Projetos em Python GITHUB/Número é ou não Primo.1!.py
+: 2
+1 
+O número 1 foi dividido 2
+NÃO é PRIMO!!
+>>> 
+= RESTART: C:/Users/maiko/OneDrive/Documentos/Meus Projetos em Python GITHUB/Número é ou não Primo.1!.py
+: 2
+1 2 
+O número testado 2, foi dividido por 2.
+PRIMO!
